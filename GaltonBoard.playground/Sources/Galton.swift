78b84919ca203e9f1c7ad1d869a01a5375bddf94
Mariana Avezum <m.avezum@tum.de>
2020-01-15 17:19:20 +0100

    Refactored position calculation and pin diameter

--- GaltonBoard.playground/Sources/Galton.swift
+++ GaltonBoard.playground/Sources/Galton.swift
@@ -3,14 +3,15 @@ import SpriteKit
 
 public class Galton{
     let totalWidth = 450 //450
-    let totalHeight = 450 //800
+    let totalHeight = 800 //800
     let capDiameter : CGFloat = 30.0
+    let pinDiameter : CGFloat = 24.0
     let levels = 5
     
     public init(){}
     
     func drawUpperTriangleIn(scene: GaltonScene){
-        drawUpperTriangle(scene: scene, levels: levels, space: capDiameter*2)
+        drawUpperTriangle(scene: scene, levels: levels)
     }
     
     func restartDropIn(scene: GaltonScene){
@@ -30,20 +31,26 @@ public class Galton{
     }
 
     func dropRandomCapInScene(scene: GaltonScene){
-        let offset = Int.random(in: -3...3)
-        let center = CGPoint(x:180+offset, y:270)
+        let offset = CGFloat.random(in: -3...3)
+        var center = positionAt(level: levels-1, ball: 1)
+        center.x = center.x+offset
         scene.addSpriteAtPosition(point: center, color: .blue, isDynamic: true, radius: capDiameter/2)
     }
 
-    func drawUpperTriangle(scene: GaltonScene, levels: Int, space:CGFloat){
+    func drawUpperTriangle(scene: GaltonScene, levels: Int){
         for i in 0...levels-2{ //for each level i
             let ballsInLvel = levels-i
             for j in 1...ballsInLvel{ //for each ball
-                scene.addSpriteAtPosition(point: CGPoint(x: (CGFloat(j)*(space))+(capDiameter*CGFloat(i)),
-                                                   y: (CGFloat(i)*(capDiameter*2))+capDiameter), color: .black, isDynamic: false, radius: 12)
+                scene.addSpriteAtPosition(point: positionAt(level: i, ball: j), color: .black, isDynamic: false, radius: pinDiameter/2)
             }
         }
     }
 
+    func positionAt(level: Int, ball:Int) -> CGPoint{
+        let space = capDiameter*2
+        let x = (CGFloat(ball)*(space))+(capDiameter*CGFloat(level))
+        let y = (CGFloat(level)*(capDiameter*2))+capDiameter
+        return CGPoint(x:x, y:y)
+    }
 }
 
